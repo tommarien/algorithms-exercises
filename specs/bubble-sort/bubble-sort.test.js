0d1795a07d21620c2b0b22d708c1d3f15d423c19
Tom Marien <tommarien@gmail.com>
2023-05-18 10:11:10 +0200

    completed bubblesort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,13 +11,38 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let maxIndex = nums.length;
+  let swapped;
+
+  do {
+    swapped = false;
+    maxIndex--;
+
+    // As higher numbers bubble to the end we need to loop through less
+    for (let index = 0; index < maxIndex; index++) {
+      const element = nums[index];
+      const nextElement = nums[index + 1];
+      if (element > nextElement) {
+        nums[index] = nextElement;
+        nums[index + 1] = element;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+
+test("bubble sort", function () {
+  const nums = [10, 5];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([5, 10]);
 });
